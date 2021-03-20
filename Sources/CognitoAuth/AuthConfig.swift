d11e2957edb2325b1a9faf860ee012f8c5254cfe
Nicholas Cross <nicholas@nacrossweb.com>
2021-03-20 20:11:08 +1100

    Expose config struct initialiser

--- Sources/CognitoAuth/AuthConfig.swift
+++ Sources/CognitoAuth/AuthConfig.swift
@@ -5,4 +5,11 @@ public struct AuthConfig {
     public let clientId: String
     public let clientSecret: String
     public let poolId: String
+
+    public init(endpointURL: URL, clientId: String, clientSecret: String, poolId: String) {
+        self.endpointURL = endpointURL
+        self.clientId = clientId
+        self.clientSecret = clientSecret
+        self.poolId = poolId
+    }
 }
